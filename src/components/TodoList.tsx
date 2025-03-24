869b14c5675d7e82a53a3e5500ba56b7af9fcd32
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-03-24 21:01:51 +0530

    feat: show completed and total task counts

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,14 +6,19 @@ type TodoListProps = {
   todos: TodoItemType[];
   setTodos: React.Dispatch<React.SetStateAction<TodoItemType[]>>;
 };
-export default function TodoList({
-  todos,
-  setTodos,
-}: TodoListProps) {
+export default function TodoList({ todos, setTodos }: TodoListProps) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
-        <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
+      {sortedTodos.map((item) => (
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );

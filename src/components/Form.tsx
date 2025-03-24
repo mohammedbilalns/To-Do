8f684acf1d3950840907624467f2a714dabdb2f0
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2025-03-24 19:21:18 +0530

    updated validations

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
-import styles from "./form.module.css"
+import styles from "./form.module.css";
 type TodoProp = {
-	todos: string[];
-	setTodos: React.Dispatch<React.SetStateAction<string[]>>;
+  todos: string[];
+  setTodos: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 export default function Form({ todos, setTodos }: TodoProp) {
-	const [todo, setTodo] = useState<string>("");
+  const [todo, setTodo] = useState<string>("");
 
-	function addTodo(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		if (todo.trim() !== "" && !todos.includes(todo)) {
-			setTodos([...todos, todo]);
-		}
-		setTodo("");
-	}
-	return (
-		<form onSubmit={addTodo}>
-			<div className={styles.inputContainer} >
-				<input
-					onChange={(e) => setTodo(e.target.value)}
-					value={todo}
-					type="text"
-					className={styles.inputBox}
-				/>
-				<button className={styles.addButton} type="submit">Add</button>
-			</div>
-
-		</form>
-	);
+  function addTodo(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (todo.trim() == "") {
+      alert("To-do item cannot be empty.");
+      return;
+    }
+    if (todos.includes(todo.trim())) {
+      alert("This item already exists");
+      return;
+    }
+    setTodos([...todos, todo.trim()]);
+    setTodo("");
+  }
+  return (
+    <form onSubmit={addTodo}>
+      <div className={styles.inputContainer}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+          className={styles.inputBox}
+        />
+        <button className={styles.addButton} type="submit">
+          Add
+        </button>
+      </div>
+    </form>
+  );
 }
